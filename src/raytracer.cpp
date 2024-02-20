69da762a9038c9afbdfc1d8caf9db72324e77760
Neokoldstudio <nightmare.vid@gmail.com>
2024-02-19 19:19:42 -0500

    IT'S WORKING ! RAYS ARE ZOOMIN'

--- src/raytracer.cpp
+++ src/raytracer.cpp
@@ -5,21 +5,30 @@ void Raytracer::render(const Scene& scene, Frame* output)
     // Crée le z_buffer.
     double *z_buffer = new double[scene.resolution[0] * scene.resolution[1]];
     for(int i = 0; i < scene.resolution[0] * scene.resolution[1]; i++) {
-        z_buffer[i] = scene.camera.z_far; //Anciennement DBL_MAX. À remplacer avec la valeur de scene.camera.z_far
+        z_buffer[i] = scene.camera.z_far;
     }
 
 	// @@@@@@ VOTRE CODE ICI
 	// Calculez les paramètres de la caméra pour les rayons.
-	
+    double3 cameraPos = scene.camera.position;
+    double fov = scene.camera.fovy;
+    double aspect_ratio = scene.resolution[0]/scene.resolution[1];
+    double viewport_height = scene.camera.z_near * tan(deg2rad(fov*0.5))*2;
+    double viewport_width = viewport_height * aspect_ratio;
+
+    double3 bottomLeftLocal = double3 {-viewport_width/2, -viewport_height/2, scene.camera.z_near};
+
+    double jittering_radius = scene.jitter_radius;
 
     // Itère sur tous les pixels de l'image.
-    for(int y = 0; y < scene.resolution[1]; y++) {
+    for (int y = 0; y < scene.resolution[1]; y++) {
 		if (y % 40){
 			std::cout << "\rScanlines completed: " << y << "/" << scene.resolution[1] << '\r';
 		}
 
         for(int x = 0; x < scene.resolution[0]; x++) {
 
+            // generate random offset for jittering
 			int avg_z_depth = 0;
 			double3 avg_ray_color{0,0,0};
 			
@@ -34,7 +43,24 @@ void Raytracer::render(const Scene& scene, Frame* output)
 				// @@@@@@ VOTRE CODE ICI
 				// Mettez en place le rayon primaire en utilisant les paramètres de la caméra.
 				// Lancez le rayon de manière uniformément aléatoire à l'intérieur du pixel dans la zone délimité par jitter_radius. 
-				//Faites la moyenne des différentes couleurs obtenues suite à la récursion.
+				// Faites la moyenne des différentes couleurs obtenues suite à la récursion.
+                double2 randomOffset = random_in_unit_disk();
+                double ray_depth_out;
+                double deltaX = x/scene.resolution[0];
+                double deltaY = y/scene.resolution[1];
+
+                double3 viewportPixelCoord = bottomLeftLocal + double3 {viewport_width * deltaX + randomOffset.x
+                                                                      ,viewport_height * deltaY + randomOffset.y, 1.0};
+
+                double3 worldPixelCoord = cameraPos + cameraPos.x * viewportPixelCoord.x
+                                          + cameraPos.y * viewportPixelCoord.y
+                                          + cameraPos.z * viewportPixelCoord.z;
+                ray.origin = cameraPos;
+                ray.direction = normalize(worldPixelCoord - cameraPos);
+                trace(scene, ray, ray_depth, &ray_color, &ray_depth_out);
+
+                avg_ray_color += ray_color;
+                avg_z_depth += (int)ray_depth_out;
 			}
 
 			avg_z_depth = avg_z_depth / scene.samples_per_pixel;
@@ -65,7 +91,7 @@ void Raytracer::render(const Scene& scene, Frame* output)
 //			  Ajouter réflexion selon material.reflection +
 //			  Ajouter réfraction selon material.refraction 
 //            pour la couleur de sortie.
-//          - Mettre à jour la nouvelle profondeure.
+//          - Mettre à jour la nouvelle profondeur.
 void Raytracer::trace(const Scene& scene,
 					  Ray ray, int ray_depth,
 					  double3* out_color, double* out_z_depth)
@@ -85,8 +111,8 @@ void Raytracer::trace(const Scene& scene,
 		//
 		// Toutes les géométries sont des surfaces et non pas de volumes.
 
-		// *out_color = 
-		// *out_z_depth =
+		*out_color = double3{255, 0, 255};
+		*out_z_depth = scene.camera.z_near + 0.01;
 	}
 }
 
