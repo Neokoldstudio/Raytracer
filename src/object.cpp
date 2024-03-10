48c94191ac8a170feb06f743ccf502548c3b28ad
Paul Godbert <paul.godbert@etu.u-bordeaux.fr>
2024-03-10 07:00:58 +0100

    all intersection detections are working :))

--- src/object.cpp
+++ src/object.cpp
@@ -35,7 +35,6 @@ bool Sphere::local_intersect(Ray ray,
     hit->depth = root;
     hit->position = ray.origin + root*ray.direction;
     hit->normal = normalize(hit->position);
-    //std::cout << "sphere hit at : (" << hit->position.x << "," << hit->position.y << "," << hit->position.z << ")" << std::endl;
     return true;
 }
 
@@ -56,7 +55,25 @@ bool Quad::local_intersect(Ray ray,
 							double t_min, double t_max, 
 							Intersection *hit)
 {
-	return false;
+    double t = -ray.origin.z / ray.direction.z;
+
+    if (t < t_min || t > t_max) {
+        return false;
+    }
+
+    double3 point = { ray.origin.x + t * ray.direction.x, ray.origin.y + t * ray.direction.y, 0.0 };
+
+    if (std::abs(point.x) > half_size || std::abs(point.y) > half_size) {
+        return false;
+    }
+
+    double3 normal = { 0.0, 0.0, 1.0 };
+
+    hit->depth = t;
+    hit->position = point;
+    hit->normal = normalize(normal);
+
+    return true;
 }
 
 // @@@@@@ VOTRE CODE ICI
@@ -77,7 +94,45 @@ bool Cylinder::local_intersect(Ray ray,
 							   double t_min, double t_max, 
 							   Intersection *hit)
 {
-    return false;
+    double a = ray.direction.x * ray.direction.x + ray.direction.z * ray.direction.z;
+    double b = 2 * (ray.origin.x * ray.direction.x + ray.origin.z * ray.direction.z);
+    double c = ray.origin.x * ray.origin.x + ray.origin.z * ray.origin.z - radius * radius;
+
+    // Résolution de l'équation quadratique pour trouver les points d'intersection
+    double discriminant = b * b - 4 * a * c;
+
+    if (discriminant < 0) {
+        return false;
+    }
+
+    auto sqrt_discriminant = sqrt(discriminant);
+    auto root = (-b - sqrt_discriminant) / (2.0 * a);
+    auto root2 = (-b + sqrt_discriminant) / (2.0 * a);
+
+    if (root <= t_min || root >= t_max) {
+        if (root2 < t_min || root2 > t_max) {
+            return false;
+        }
+    }
+
+    double3 point = ray.origin + root*ray.direction;
+    // Vérification de l'appartenance du point au cylindre (si le premier point est trop "loin", check le deuxième)
+    // évite des problèmes qui pourrait s'apparenter à du backface culling (ce n'en est pas en réalité, l'effet est trompeur)
+    if (point.y < -half_height || point.y > half_height) {
+        root = root2;
+        point = ray.origin + root * ray.direction;
+        if (point.y < -half_height || point.y > half_height) {
+            return false;
+        }
+    }
+
+    double3 normal = {point.x, 0.0, point.z};
+
+    hit->depth = root;
+    hit->position = point;
+    hit->normal = normalize(normal);
+
+    return true;
 }
 
 // @@@@@@ VOTRE CODE ICI
@@ -98,7 +153,20 @@ bool Mesh::local_intersect(Ray ray,
 						   double t_min, double t_max, 
 						   Intersection* hit)
 {
-	return false;
+	bool intersected = false;
+
+    // Parcourir chaque triangle du maillage et tester s'il y a une intersection
+    for (size_t i = 0; i < triangles.size(); ++i)
+    {
+        if (intersect_triangle(ray, t_min, t_max, triangles[i], hit))
+        {
+            // Mettre à jour t_max avec la profondeur de l'intersection actuelle
+            t_max = hit->depth;
+            intersected = true;
+        }
+    }
+
+    return intersected;
 }
 
 // @@@@@@ VOTRE CODE ICI
@@ -132,7 +200,49 @@ bool Mesh::intersect_triangle(Ray  ray,
 	// NOTE : hit.depth est la profondeur de l'intersection actuellement la plus proche,
 	// donc n'acceptez pas les intersections qui occurent plus loin que cette valeur.
 
-	return false;
+	// Calcul du vecteur de l'arête BA et de l'arête AC
+	double3 AB = p1 - p0;
+	double3 AC = p2 - p0;
+
+	// Calcul du produit vectoriel de l'arrête AC et du vecteur de direction du rayon
+	double3 h = cross(ray.direction, AC);
+    // On projette le vecteur h sur l'arrête AB
+	double a = dot(AB, h);
+
+	// Si a est proche de zéro, le rayon est parallèle au triangle
+	if (a > -EPSILON && a < EPSILON)
+		return false;
+
+	double f = 1.0 / a;
+	double3 s = ray.origin - p0;
+	double u = f * dot(s, h);
+
+	if (u < 0.0 || u > 1.0)
+		return false;
+
+	double3 q = cross(s, AB);
+	double v = f * dot(ray.direction, q);
+
+	if (v < 0.0 || u + v > 1.0)
+		return false;
+
+	// Calcul de t pour trouver l'intersection du rayon et du triangle
+	double t = f * dot(AC, q);
+
+	// Vérifier si l'intersection est dans la plage spécifiée
+	if (t < t_min || t > t_max)
+		return false;
+
+	// Si l'intersection est plus profonde que la valeur actuelle de hit->depth, retourner false -> on ne veut pas override une intersection plus proche
+	if (hit->depth < t)
+		return false;
+
+	// Mettre à jour les informations de l'intersection
+	hit->depth = t;
+	hit->normal = normalize(cross(AB, AC));
+	hit->position = ray.origin + t * ray.direction;
+
+	return true;
 }
 
 // @@@@@@ VOTRE CODE ICI
